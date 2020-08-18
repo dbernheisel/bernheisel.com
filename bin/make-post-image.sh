660d1eada1f480152442374230db9ce68c346877
David Bernheisel <david@bernheisel.com>
2020-08-18 02:12:05 -0400

    Add blog post images

--- bin/make-post-image.sh
+++ bin/make-post-image.sh
@@ -10,8 +10,8 @@ mkdir ./assets/static/images/blog &>/dev/null || true
 convert "./assets/static/images/pattern-wide.png" \
   \( -background none -fill white -size 320x -font ./assets/static/fonts/FiraCode-SemiBold.ttf \
      label:"bernheisel.com" \
-  \) -gravity southeast -geometry +0+0 -compose over -composite \
-  \( -background none -fill white -size 963x381 -font ./assets/static/fonts/Inter-SemiBold.otf \
-     -size 963x381 caption:"$TITLE" \
-  \) -gravity northwest -geometry +0+0 -compose over -composite \
+  \) -gravity southeast -geometry +40+20 -compose over -composite \
+  \( -background none -fill white -size 900x381 -font ./assets/static/fonts/Inter-SemiBold.otf \
+     -size 900x381 caption:"$TITLE" \
+  \) -gravity northeast -geometry +40+0 -compose over -composite \
   "./assets/static/images/blog/$ID.png"
