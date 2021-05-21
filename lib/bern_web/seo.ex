a98a60fb9d2c33503827054880865c1dd960001e
David Bernheisel <david@bernheisel.com>
2021-05-21 12:34:30 -0400

    Use Original Publishing URL as canonical url

--- lib/bern_web/seo.ex
+++ lib/bern_web/seo.ex
@@ -4,18 +4,26 @@ defmodule BernWeb.SEO do
   use BernWeb, :view
   alias BernWeb.SEO.{Generic, Breadcrumbs, OpenGraph}
 
-  @default_assigns %{site: %Generic{}, breadcrumbs: nil, og: nil}
+  @default_assigns %{canonical_url: nil, site: %Generic{}, breadcrumbs: nil, og: nil}
 
   def meta(conn, BernWeb.Live.BlogShow, %{post: post}) do
     render(
       "meta.html",
       @default_assigns
+      |> put_canonical(conn, post)
       |> put_opengraph_tags(conn, post)
       |> put_breadcrumbs(conn, post)
     )
   end
 
-  def meta(_, _, _), do: render("meta.html", @default_assigns)
+  def meta(conn, _view, _assigns),
+    do: render("meta.html", @default_assigns |> put_canonical(conn, nil))
+
+  def put_canonical(assigns, _conn, %{original_url: url}) when url not in ["", nil],
+    do: Map.put(assigns, :canonical_url, url)
+
+  def put_canonical(assigns, conn, _post),
+    do: Map.put(assigns, :canonical_url, "https://bernheisel.com#{Phoenix.Controller.current_path(conn)}")
 
   def put_opengraph_tags(assigns, conn, event),
     do: Map.put(assigns, :og, OpenGraph.build(conn, event))
